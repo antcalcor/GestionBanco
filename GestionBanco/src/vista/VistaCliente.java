04c43272c42f12d8fae1ba0d6cf3cbc2c9a0fb0e
Antonio Calderon <carde46@hotmail.com>
2018-10-06 22:01:48 +0200

    haciendo vistas

--- GestionBanco/src/vista/VistaCliente.java
+++ GestionBanco/src/vista/VistaCliente.java
@@ -27,18 +27,252 @@ public class VistaCliente extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        buttonGroup1 = new javax.swing.ButtonGroup();
+        jDialog1 = new javax.swing.JDialog();
+        jButton6 = new javax.swing.JButton();
+        jButton7 = new javax.swing.JButton();
+        jButton8 = new javax.swing.JButton();
+        jButton9 = new javax.swing.JButton();
+        jScrollPane4 = new javax.swing.JScrollPane();
+        jTable3 = new javax.swing.JTable();
+        jLabel10 = new javax.swing.JLabel();
+        jFormattedTextField8 = new javax.swing.JFormattedTextField();
+        jLabel11 = new javax.swing.JLabel();
+        jFormattedTextField9 = new javax.swing.JFormattedTextField();
+        jLabel12 = new javax.swing.JLabel();
+        jFormattedTextField10 = new javax.swing.JFormattedTextField();
+        jLabel13 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jFormattedTextField1 = new javax.swing.JFormattedTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jFormattedTextField2 = new javax.swing.JFormattedTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jFormattedTextField3 = new javax.swing.JFormattedTextField();
+        jLabel3 = new javax.swing.JLabel();
+        jFormattedTextField4 = new javax.swing.JFormattedTextField();
+        jLabel4 = new javax.swing.JLabel();
+        jFormattedTextField5 = new javax.swing.JFormattedTextField();
+        jLabel5 = new javax.swing.JLabel();
+        jFormattedTextField6 = new javax.swing.JFormattedTextField();
+        jLabel6 = new javax.swing.JLabel();
+        jRadioButton1 = new javax.swing.JRadioButton();
+        jRadioButton2 = new javax.swing.JRadioButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable2 = new javax.swing.JTable();
+        jLabel7 = new javax.swing.JLabel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList<>();
+        jLabel8 = new javax.swing.JLabel();
+        jFormattedTextField7 = new javax.swing.JFormattedTextField();
+        jLabel9 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        jDialog1.getContentPane().setLayout(null);
+
+        jButton6.setText("Añadir");
+        jDialog1.getContentPane().add(jButton6);
+        jButton6.setBounds(20, 280, 67, 32);
+
+        jButton7.setText("Modificar");
+        jDialog1.getContentPane().add(jButton7);
+        jButton7.setBounds(100, 280, 83, 32);
+
+        jButton8.setText("Eliminar");
+        jDialog1.getContentPane().add(jButton8);
+        jButton8.setBounds(190, 280, 76, 32);
+
+        jButton9.setText("Volver");
+        jDialog1.getContentPane().add(jButton9);
+        jButton9.setBounds(270, 280, 66, 32);
+
+        jTable3.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane4.setViewportView(jTable3);
+
+        jDialog1.getContentPane().add(jScrollPane4);
+        jScrollPane4.setBounds(40, 50, 280, 110);
+
+        jLabel10.setText("Direcciones");
+        jDialog1.getContentPane().add(jLabel10);
+        jLabel10.setBounds(140, 20, 70, 16);
+
+        jFormattedTextField8.setText("jFormattedTextField1");
+        jDialog1.getContentPane().add(jFormattedTextField8);
+        jFormattedTextField8.setBounds(170, 240, 150, 20);
+
+        jLabel11.setText("Ciudad");
+        jDialog1.getContentPane().add(jLabel11);
+        jLabel11.setBounds(170, 220, 120, 16);
+
+        jFormattedTextField9.setText("jFormattedTextField1");
+        jDialog1.getContentPane().add(jFormattedTextField9);
+        jFormattedTextField9.setBounds(20, 240, 130, 20);
+
+        jLabel12.setText("Codigo Postal");
+        jDialog1.getContentPane().add(jLabel12);
+        jLabel12.setBounds(20, 220, 90, 16);
+
+        jFormattedTextField10.setText("jFormattedTextField1");
+        jDialog1.getContentPane().add(jFormattedTextField10);
+        jFormattedTextField10.setBounds(20, 190, 290, 20);
+
+        jLabel13.setText("Calle");
+        jDialog1.getContentPane().add(jLabel13);
+        jLabel13.setBounds(20, 170, 40, 16);
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(740, 510));
+        getContentPane().setLayout(null);
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        getContentPane().add(jScrollPane1);
+        jScrollPane1.setBounds(60, 40, 610, 210);
+
+        jFormattedTextField1.setText("jFormattedTextField1");
+        getContentPane().add(jFormattedTextField1);
+        jFormattedTextField1.setBounds(20, 330, 150, 20);
+
+        jLabel1.setText("Codigo Cliente");
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(20, 310, 90, 16);
+
+        jFormattedTextField2.setText("jFormattedTextField1");
+        getContentPane().add(jFormattedTextField2);
+        jFormattedTextField2.setBounds(20, 280, 340, 20);
+
+        jLabel2.setText("Nombre");
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(20, 260, 50, 16);
+
+        jFormattedTextField3.setText("jFormattedTextField1");
+        getContentPane().add(jFormattedTextField3);
+        jFormattedTextField3.setBounds(20, 430, 140, 20);
+
+        jLabel3.setText("Sexo");
+        getContentPane().add(jLabel3);
+        jLabel3.setBounds(20, 410, 29, 16);
+
+        jFormattedTextField4.setText("jFormattedTextField1");
+        getContentPane().add(jFormattedTextField4);
+        jFormattedTextField4.setBounds(20, 380, 140, 20);
+
+        jLabel4.setText("Direcciones");
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(410, 260, 70, 16);
+
+        jFormattedTextField5.setText("jFormattedTextField1");
+        getContentPane().add(jFormattedTextField5);
+        jFormattedTextField5.setBounds(410, 380, 150, 20);
+
+        jLabel5.setText("Telefono");
+        getContentPane().add(jLabel5);
+        jLabel5.setBounds(200, 310, 60, 16);
+
+        jFormattedTextField6.setText("jFormattedTextField1");
+        getContentPane().add(jFormattedTextField6);
+        jFormattedTextField6.setBounds(200, 380, 160, 20);
+
+        jLabel6.setText("Representante");
+        getContentPane().add(jLabel6);
+        jLabel6.setBounds(200, 360, 90, 16);
+
+        buttonGroup1.add(jRadioButton1);
+        jRadioButton1.setText("Personas Juridicas");
+        getContentPane().add(jRadioButton1);
+        jRadioButton1.setBounds(350, 10, 160, 28);
+
+        buttonGroup1.add(jRadioButton2);
+        jRadioButton2.setText("Personas Fisicas");
+        getContentPane().add(jRadioButton2);
+        jRadioButton2.setBounds(180, 10, 140, 28);
+
+        jTable2.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane2.setViewportView(jTable2);
+
+        getContentPane().add(jScrollPane2);
+        jScrollPane2.setBounds(410, 280, 280, 70);
+
+        jLabel7.setText("Tipo Organizacion");
+        getContentPane().add(jLabel7);
+        jLabel7.setBounds(20, 360, 110, 16);
+
+        jList1.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane3.setViewportView(jList1);
+
+        getContentPane().add(jScrollPane3);
+        jScrollPane3.setBounds(270, 310, 110, 40);
+
+        jLabel8.setText("Numero Empleados");
+        getContentPane().add(jLabel8);
+        jLabel8.setBounds(410, 360, 120, 16);
+
+        jFormattedTextField7.setText("jFormattedTextField1");
+        getContentPane().add(jFormattedTextField7);
+        jFormattedTextField7.setBounds(200, 430, 140, 20);
+
+        jLabel9.setText("Fecha Nacimiento");
+        getContentPane().add(jLabel9);
+        jLabel9.setBounds(200, 410, 110, 16);
+
+        jButton1.setText("Añadir");
+        getContentPane().add(jButton1);
+        jButton1.setBounds(370, 420, 67, 32);
+
+        jButton2.setText("Modificar");
+        getContentPane().add(jButton2);
+        jButton2.setBounds(454, 420, 83, 32);
+
+        jButton3.setText("Eliminar");
+        getContentPane().add(jButton3);
+        jButton3.setBounds(550, 420, 76, 32);
+
+        jButton4.setText("Volver");
+        getContentPane().add(jButton4);
+        jButton4.setBounds(640, 420, 66, 32);
+
+        jButton5.setText("+");
+        getContentPane().add(jButton5);
+        jButton5.setBounds(650, 360, 40, 32);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -79,5 +313,49 @@ public class VistaCliente extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    public javax.swing.ButtonGroup buttonGroup1;
+    public javax.swing.JButton jButton1;
+    public javax.swing.JButton jButton2;
+    public javax.swing.JButton jButton3;
+    public javax.swing.JButton jButton4;
+    public javax.swing.JButton jButton5;
+    public javax.swing.JButton jButton6;
+    public javax.swing.JButton jButton7;
+    public javax.swing.JButton jButton8;
+    public javax.swing.JButton jButton9;
+    public javax.swing.JDialog jDialog1;
+    public javax.swing.JFormattedTextField jFormattedTextField1;
+    public javax.swing.JFormattedTextField jFormattedTextField10;
+    public javax.swing.JFormattedTextField jFormattedTextField2;
+    public javax.swing.JFormattedTextField jFormattedTextField3;
+    public javax.swing.JFormattedTextField jFormattedTextField4;
+    public javax.swing.JFormattedTextField jFormattedTextField5;
+    public javax.swing.JFormattedTextField jFormattedTextField6;
+    public javax.swing.JFormattedTextField jFormattedTextField7;
+    public javax.swing.JFormattedTextField jFormattedTextField8;
+    public javax.swing.JFormattedTextField jFormattedTextField9;
+    public javax.swing.JLabel jLabel1;
+    public javax.swing.JLabel jLabel10;
+    public javax.swing.JLabel jLabel11;
+    public javax.swing.JLabel jLabel12;
+    public javax.swing.JLabel jLabel13;
+    public javax.swing.JLabel jLabel2;
+    public javax.swing.JLabel jLabel3;
+    public javax.swing.JLabel jLabel4;
+    public javax.swing.JLabel jLabel5;
+    public javax.swing.JLabel jLabel6;
+    public javax.swing.JLabel jLabel7;
+    public javax.swing.JLabel jLabel8;
+    public javax.swing.JLabel jLabel9;
+    private javax.swing.JList<String> jList1;
+    public javax.swing.JRadioButton jRadioButton1;
+    public javax.swing.JRadioButton jRadioButton2;
+    public javax.swing.JScrollPane jScrollPane1;
+    public javax.swing.JScrollPane jScrollPane2;
+    public javax.swing.JScrollPane jScrollPane3;
+    public javax.swing.JScrollPane jScrollPane4;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTable jTable2;
+    private javax.swing.JTable jTable3;
     // End of variables declaration//GEN-END:variables
 }
